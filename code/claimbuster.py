aec2e2c7e222d22b89da130b80691a242dd5d803
Robbert <dehaven.robbert@gmail.com>
2018-12-10 20:43:30 +0100

    expanded claimbuster.py to sort for top and percentages

--- code/claimbuster.py
+++ code/claimbuster.py
@@ -22,7 +22,7 @@ def split_text(text):
             block_nr += 1;
     return text_blocks
 
-def get_CB_score(p):
+def query_api(p):
     query = "https://idir-server2.uta.edu:443/factchecker/score_text/" + p
     # Make a get request 
     response = requests.get(query)
@@ -31,10 +31,48 @@ def get_CB_score(p):
     print(json.dumps(response, indent=4, sort_keys=True))
     return response
 
-article = dataIO.readData(2)
-print article.get('article')
-text = article.get('article').decode('utf-8')
-splitT = split_text(text)
-print splitT
-for block in splitT:
-    get_CB_score(block)
+def get_CB_score():
+    item = dataIO.readData(2)
+    # print item.get('article')
+    text = item.get('article').decode('utf-8')
+    splitT = split_text(text)
+    # print splitT
+
+    total_scored_article = []
+    for block in splitT:
+        scored_item = query_api(block)
+        for sentence in scored_item.get('results'):
+            total_scored_article.append(sentence)
+    return total_scored_article
+
+def get_CB_thesholded_article(threshold):
+    scored_article = get_CB_score()
+    out = ''
+    for sentence in scored_article:
+        if float(sentence.get('score')) > threshold:
+            out += sentence.get('text')
+    return out
+
+def get_CB_thesholded_article_top(amount_sentences):
+    scored_article = get_CB_score()
+    sorted_article = sorted(scored_article, key=lambda k: k['score'], reverse=True )
+    threshold = float(sorted_article[amount_sentences-1].get('score'))
+    out = ''
+    for sentence in scored_article:
+        if float(sentence.get('score')) >= threshold:
+            out += sentence.get('text')
+    return out
+
+def get_CB_thesholded_article_perc(percentage):
+    scored_article = get_CB_score()
+    sorted_article = sorted(scored_article, key=lambda k: k['score'], reverse=True )
+    amount_sentences = int(round(percentage*len(scored_article), 0))
+    threshold = float(sorted_article[amount_sentences-1].get('score'))
+    out = ''
+    for sentence in scored_article:
+        if float(sentence.get('score')) >= threshold:
+            out += sentence.get('text')
+    return out
+
+
+print get_CB_thesholded_article_perc(0.1)
