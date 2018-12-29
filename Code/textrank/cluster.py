64269981ae4dc0fb53326189dcc6704bac7be331
ANH <TRAN.TuanAnh.vn93@gmail.com>
2018-12-29 18:18:46 +0100

    cluster

--- Code/textrank/cluster.py
+++ Code/textrank/cluster.py
@@ -4,22 +4,23 @@ from scipy.cluster import  hierarchy
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import squareform
 
-def cluster(sentences, similarity_matrix, threshold = 0.8):
-    cluster = pd.DataFrame(data=sentences, columns=["sentence"])
-    cluster['cluster'] = None
-    n = len(sentences)    
+def manual_cluster(df_sent, similarity_matrix, threshold = 0.8):
+    df_cluster = df_sent
+    df_cluster['cluster'] = None
+    n = similarity_matrix.shape[0]  
     clust = 0
     flag_clust = False
-    for i in range(0,n):
-        for j in range(i+1,n):
-            if similarity_matrix[i, j] >= threshold and cluster.loc[i,'cluster'] is None and cluster.loc[j,'cluster'] is None:
-                cluster.loc[[i,j], 'cluster'] = clust
-                flag_clust = True
-        if flag_clust:
-            clust += 1
-            flag_clust = False   
-    cluster.loc[pd.isna(cluster["cluster"]), 'cluster'] = clust
-    return cluster
+    for i in range(0,n-1):
+        if df_cluster.loc[i,'cluster'] is None:
+            for j in range(i+1,n):
+                if similarity_matrix[i, j] >= threshold:
+                    df_cluster.loc[[i,j], 'cluster'] = clust
+                    flag_clust = True
+            if flag_clust:
+                clust += 1
+                flag_clust = False   
+#    df_cluster.loc[pd.isna(df_cluster["cluster"]), 'cluster'] = clust
+    return df_cluster
 
 
 if __name__ == '__main__':
@@ -38,15 +39,23 @@ if __name__ == '__main__':
     for i in range(0,20):
         for j in range(0,20):
             similarity_matrix[i,j] = matrix.loc[(matrix["id1"] == i) & (matrix["id2"] == j)]["score"].values[0]
-            
-    labelList = list_sent_id[0:20]      
-    linked = hierarchy.linkage(similarity_matrix)
-    plt.figure(figsize=(10,len(labelList)/2))  
-    R = hierarchy.dendrogram(linked,  
-                        orientation='left',
-                        labels=labelList,
-                        distance_sort='descending',
-                        show_leaf_counts=True,
-                        leaf_font_size = 12
-                        )
-    plt.show()
+    cluster_result = manual_cluster(df_sent[0:20], similarity_matrix) 
+     
+#    HIERARCHY CLUSTERING   
+#    labelList = list_sent_id[0:20]      
+#    linked = hierarchy.linkage(similarity_matrix)
+#    plt.figure(figsize=(10,len(labelList)/2))  
+#    R = hierarchy.dendrogram(linked,  
+#                        orientation='left',
+#                        labels=labelList,
+#                        distance_sort='descending',
+#                        show_leaf_counts=True,
+#                        leaf_font_size = 12
+#                        )
+#    plt.show()
+     
+      
+     
+     
+     
+     
