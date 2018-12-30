f6b8c5d137e17f3392fd3d45929597bebeea240e
ANH <TRAN.TuanAnh.vn93@gmail.com>
2018-12-30 13:20:45 +0100

    fix clustering method

--- Code/textrank/cluster.py
+++ Code/textrank/cluster.py
@@ -3,29 +3,27 @@ import numpy as np
 from scipy.cluster import  hierarchy
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import squareform
+import collections
 
-def manual_cluster(df_sent, similarity_matrix, threshold = 0.8):
-    df_cluster = df_sent
-    df_cluster['cluster'] = None
-    n = similarity_matrix.shape[0]  
-    clust = 0
-    flag_clust = False
-    for i in range(0,n-1):
-        if df_cluster.loc[i,'cluster'] is None:
-            for j in range(i+1,n):
-                if similarity_matrix[i, j] >= threshold:
-                    df_cluster.loc[[i,j], 'cluster'] = clust
-                    flag_clust = True
-            if flag_clust:
-                clust += 1
-                flag_clust = False   
-#    df_cluster.loc[pd.isna(df_cluster["cluster"]), 'cluster'] = clust
-    return df_cluster
+def hierarchy_cluster(df_sent, similarity_matrix, threshold = 0.1):
+    df = df_sent
+    invert_matrix = 1 - similarity_matrix
+    dists = squareform(invert_matrix)    
+    linked = hierarchy.linkage(dists, 'single')
+    assignments = hierarchy.fcluster(linked, threshold, 'distance')
+    df['cluster'] = assignments 
+    df['count'] = df.groupby('cluster')['cluster'].transform('count')
+#    df.loc[df["count"] == 1, "cluster"] = None
+#    df = df.drop(["count"], axis=1)
+    df = df.loc[df["count"] > 1]
+    return df
 
+def check_symmetric(a, tol=1e-8):
+    return np.allclose(a, a.T, atol=tol)
 
 if __name__ == '__main__':
-    input_path = "../../Results/similarity_matrix.tsv"
-    output_path = '../../Results/textrank'
+    input_path = "../../Results/similarity_matrix_top10_topic1.tsv"
+    output_path = '../../Results/clustering_top10_topic1.tsv'
     
     matrix = pd.read_csv(input_path, sep='\t', header=None)
     matrix.columns = ["id1", "id2", "sent1", "sent2", "score"]
@@ -33,29 +31,45 @@ if __name__ == '__main__':
     list_sent_id = matrix.loc[matrix['id1'] == 0, "id2"].tolist()
     list_sent = matrix.loc[matrix['id1'] == 0, "sent2"].tolist()
     
-    df_sent = pd.DataFrame({"id":list_sent_id, "sent":list_sent})
+    df_sent = pd.DataFrame({"id":list_sent_id, "sentence":list_sent})
     
-    similarity_matrix = np.zeros((20,20))
-    for i in range(0,20):
-        for j in range(0,20):
+    n = len(list_sent)
+    similarity_matrix = np.zeros((n,n))
+    for i in range(0,n):
+        for j in range(0,n):
             similarity_matrix[i,j] = matrix.loc[(matrix["id1"] == i) & (matrix["id2"] == j)]["score"].values[0]
-    cluster_result = manual_cluster(df_sent[0:20], similarity_matrix) 
+    
+    similarity_matrix = similarity_matrix.round(2)
+    flag_symmetric = check_symmetric(similarity_matrix)
+    
+    if flag_symmetric:
+        print("Similarity matrix is symmetric !")
+        cluster_results = hierarchy_cluster(df_sent, similarity_matrix, threshold = 0.1)
+        cluster_results.to_csv(output_path, sep='\t', index=False) 
+    else:
+        print("Similarity matrix is assymmetric !")
      
-#    HIERARCHY CLUSTERING   
-#    labelList = list_sent_id[0:20]      
-#    linked = hierarchy.linkage(similarity_matrix)
-#    plt.figure(figsize=(10,len(labelList)/2))  
-#    R = hierarchy.dendrogram(linked,  
-#                        orientation='left',
-#                        labels=labelList,
-#                        distance_sort='descending',
-#                        show_leaf_counts=True,
-#                        leaf_font_size = 12
-#                        )
-#    plt.show()
-     
-      
-     
-     
+    
+#    HIERARCHY CLUSTERING - DENDOGRAM
+#    if flag_symmetric:
+#        labelList = list_sent_id 
+#        invert_matrix = 1 - similarity_matrix
+#        dists = squareform(invert_matrix)    
+#        linked = hierarchy.linkage(dists, 'single')
+#        plt.figure(figsize=(10,len(labelList)/2))  
+#        R = hierarchy.dendrogram(linked,  
+#                            orientation='left',
+#                            labels=labelList,
+#                            distance_sort='descending',
+#                            show_leaf_counts=True,
+#                            leaf_font_size = 12
+#                            )
+#        plt.show()
+        
+        
+        
+        
+
+        
      
      
